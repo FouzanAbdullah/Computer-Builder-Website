18f3a1722f55d820256507e670244e5a4d2817b2
Carl Paladino <carlpaladino9@hotmail.com>
2023-03-21 16:18:32 -0400

    data.js

--- data.js
+++ data.js
@@ -1,7 +1,3 @@
 data = [
-    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg", "description":"A nice graphics card"},
-    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"},
-    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"},
-    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"},
-    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"}
+    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg", "description":"A nice graphics card"}
 ];
