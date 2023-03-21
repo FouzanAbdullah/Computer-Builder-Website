6ad49e45abed4d54ddd2601b6b8d7e1c9b036afb
Carl Paladino <carlpaladino9@hotmail.com>
2023-03-21 16:16:44 -0400

    details.html functions with the data

---
+++ data.js
@@ -0,0 +1,7 @@
+data = [
+    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg", "description":"A nice graphics card"},
+    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"},
+    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"},
+    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"},
+    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg"}
+];
