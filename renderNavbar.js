ab973438fadc7b282632f3333598d8b82ccbd3c9
Carl Paladino <carlpaladino9@hotmail.com>
2023-03-22 21:04:27 -0400

    renderNavbar.js

--- renderNavbar.js
+++ renderNavbar.js
@@ -20,8 +20,18 @@ function getPageName() {
 	return basename ? basename : "";
 }
 
-function makeNavChild() {
+function makeNavChild(label, linksTo, ulElement) {
+	//home nav button
+	var li = document.createElement("li");
+	li.classList.add("nav-item", "navButton");
 
+	var a = document.createElement("a");
+	a.classList.add("nav-link");
+	a.innerHTML = label;
+	a.href = linksTo;
+
+	ulElement.appendChild(li);
+	li.appendChild(a);
 }
 
 var renderNavBar = function () {
@@ -43,87 +53,15 @@ var renderNavBar = function () {
 	);
 	div.appendChild(ul);
 
-	//home nav button
-	var liHome = document.createElement("li");
-	liHome.classList.add("nav-item", "navButton");
-
-	var aHome = document.createElement("a");
-	aHome.classList.add("nav-link");
-	aHome.innerHTML = "Home";
-	aHome.href = "index.html";
-
-	//products nav button
-	var liProducts = document.createElement("li");
-	liProducts.classList.add("nav-item", "navButton");
-
-	var aProducts = document.createElement("a");
-	aProducts.classList.add("nav-link");
-	aProducts.innerHTML = "Products";
-	aProducts.href = "featured.html";
-
-	//events nav button
-	var liEvents = document.createElement("li");
-	liEvents.classList.add("nav-item", "navButton");
-
-	var aEvents = document.createElement("a");
-	aEvents.classList.add("nav-link");
-	aEvents.innerHTML = "Events";
-	aEvents.href = "events.html";
-
-	//questionnaire nav button
-	var liQuestion = document.createElement("li");
-	liQuestion.classList.add("nav-item", "navButton");
-
-	var aQuestion = document.createElement("a");
-	aQuestion.classList.add("nav-link");
-	aQuestion.innerHTML = "Questionnaire";
-	aQuestion.href = "questionnaire.html";
-
-	//quiz nav button
-	var liQuiz = document.createElement("li");
-	liQuiz.classList.add("nav-item", "navButton");
-
-	var aQuiz = document.createElement("a");
-	aQuiz.classList.add("nav-link");
-	aQuiz.innerHTML = "Quiz";
-	aQuiz.href = "quiz.html";
-
-	//assign the correct active list item in the navbar
-	var pageName = getPageName();
-	switch (pageName) {
-		case "home":
-			aHome.classList.add("active");
-			aHome.setAttribute("style", "border-bottom: 2px solid #4287f5");
-			break;
-		case "timeline":
-			aTimeline.classList.add("active");
-			aTimeline.setAttribute("style", "border-bottom: 2px solid #4287f5");
-			break;
-		case "events":
-			aEvents.classList.add("active");
-			aEvents.setAttribute("style", "border-bottom: 2px solid #4287f5");
-			break;
-		case "questionnaire":
-			aQuestion.classList.add("active");
-			aQuestion.setAttribute("style", "border-bottom: 2px solid #4287f5");
-			break;
-		case "quiz":
-			aQuiz.classList.add("active");
-			aQuiz.setAttribute("style", "border-bottom: 2px solid #4287f5");
-			break;
-	}
-
-	//add the created elements to the navbar element
-	ul.appendChild(liHome);
-	liHome.appendChild(aHome);
-	ul.appendChild(liProducts);
-	liProducts.appendChild(aProducts);
-	ul.appendChild(liEvents);
-	liEvents.appendChild(aEvents);
-	ul.appendChild(liQuestion);
-	liQuestion.appendChild(aQuestion);
-	ul.appendChild(liQuiz);
-	liQuiz.appendChild(aQuiz);
+	makeNavChild("Home", "index.html", ul);
+	makeNavChild("Products", "featured.html", ul);
+	makeNavChild("Build a PC", "build.html", ul);
+	makeNavChild("Featured", "featured.html", ul);
+	makeNavChild("Specials", "#", ul);
+	makeNavChild("My Account", "login.html", ul);
+	makeNavChild("Sign Up", "register.html", ul);
+	makeNavChild("Shipping", "#", ul);
+	makeNavChild("Contact Us", "contact.html", ul);
 
 	document.body.insertBefore(nav, document.body.firstChild);
 
