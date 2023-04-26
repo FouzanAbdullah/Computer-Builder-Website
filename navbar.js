55ed57ac2c6cd4a676f4dca9565f07c660968b89
Ibrahim Hashmi <ibhashmi@gmail.com>
2023-04-26 19:03:11 -0400

    added footer to each page

--- navbar.js
+++ navbar.js
@@ -28,21 +28,21 @@ const footerHTML=`
             <div class="footer-heading footer-1">
                 <h2>About Us</h2>
                 <a href="#">Blog</a>
-                <a href="#">Demo</a>
+                <a href="#">Retailers</a>
                 <a href="#">Customers</a>
                 <a href="#">Investors</a>
                 <a href="#">Terms of Service</a>
             </div>
             <div class="footer-heading footer-2">
                 <h2>Contact Us</h2>
-                <a href="#">Jobs</a>
+                <a href="#">Careers</a>
                 <a href="#">Support</a>
                 <a href="#">Contact</a>
                 <a href="#">Sponsorships</a>
             </div>
             <div class="footer-heading footer-3">
                 <h2>Social Media</h2>
-                <a href="#">GitHub</a>
+                <a href="https://github.com/FouzanAbdullah/COSC-3P94-Stage-2">GitHub</a>
                 <a href="#">Instagram</a>
                 <a href="#">Facebook</a>
                 <a href="#">Twitter</a>
