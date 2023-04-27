1a598170f62026ae9e4d5d8565a3aa034a158e83
Carl Paladino <carlpaladino9@hotmail.com>
2023-04-27 01:39:04 -0400

    styling details

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
 data = [
-    {"id": "nvidiartx3060", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg", "description":"A nice graphics card"},
-    {"id": "asusrogmaximus", "name":"ASUS Rog Maximus", "price":"223", "salePrice":"0", "image":"src/asusrogmaximus.jpg", "description":"A very functional motherboard that can perform well"}
+    {"id": "nvidiartx3060", "brand":"NVIDIA", "name":"Nvidia RTX 3060", "price":"350", "salePrice":"270", "image":"src/nvidiartx3060.jpg", "description":"A nice graphics card"},
+    {"id": "asusrogmaximus", "brand":"ASUS", "name":"ASUS Rog Maximus", "price":"223", "salePrice":"0", "image":"src/asusmaximus.jpg", "description":"A very functional motherboard that can perform well"}
 ];
