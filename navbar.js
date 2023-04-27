7107929d8724f5566d2a8e50fe934089b71243d7
julellisg <julianellisg@gmail.com>
2023-04-27 02:43:48 -0400

    Fixed styling and spacing on featured.html
    - removed the Special Offers tab (not being used)
    - added Choose Motherboard functionality

--- navbar.js
+++ navbar.js
@@ -14,7 +14,6 @@ const navBarHtml = `
       <a href="products.html">Browse Products</a>
       <a href="build.html">PC Builder</a>
       <a href="featured.html">Featured Builds</a>
-      <a href="#">Special Offers</a>
       <!-- <a href="#" class="right-align">Put a search bar here</a> -->
       <button type="submit" class="right-align">Search</button>
       <input type="text" placeholder="Search for hardware..." class="right-align">
